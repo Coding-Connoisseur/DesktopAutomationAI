046ba33135e054172f946b194b5f698596cc533c
Coding-Connoisseur <davidtraversmailbox@gmail.com>
2024-09-23 18:45:06 +0000

    Delete All

--- src/modules/browser_automation/__init__.py
+++
@@ -1,7 +0,0 @@
-# __init__.py
-# Initializes the browser automation module by importing necessary components.
-
-from .browser_control import open_browser, navigate_to_url, fill_form, click_element
-
-# This file ensures that when the browser_automation module is imported,
-# all its key functionalities become accessible throughout the application.
